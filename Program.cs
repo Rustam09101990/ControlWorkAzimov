39c6ae1434b2d7842cc0f7a8d739124f6c9c2654
Rustam Azimov <azimovrstm@gmail.com>
2024-02-22 19:02:50 +0300

    Создали папку и файл реадми

--- Program.cs
+++ Program.cs
@@ -1,40 +1,54 @@
-﻿
-// все натуральные числа в промежутке от m n 
-void Numbers (int m, int n)
-{
-   if (m > n)
-       return;
-    Console.Write($"{m} ");
-    Numbers(m + 1, n);
-}
-Numbers(5, 100);
-
-///Функция Аккермана 
-
-Console.Write("Введите значение M: ");
+﻿Console.Write("Введите количество массивов: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[m];
 
-int akkerman(int m, int n)
+
+void array(string[] stringArray)
 {
-    if (m == 0) return n + 1;
-    else if (n == 0) return akkerman(m - 1, 1);
-    else return akkerman(m - 1, akkerman(m, n - 1));
-}
-
-Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
-// Задача 3
-using System.Linq.Expressions;
-
-void PrintArray(int[] array, int current = 0)
-{
-
+    for (int i = 0; i < stringArray.Length; i++)
     {
-        Console.Write(" " + array[current++]);
-        PrintArray(array, current);
-    }
+        Console.WriteLine($"Введите { i + 1} элемент массива");
+    stringArray[i] = Console.ReadLine();
+}
 }
 
-int [] NewArray = [1,2,3,5,6,7,8,10];
-PrintArray(NewArray);
+
+
+
+string[] symbol(string[] stringArray)
+{
+    int n = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+            n++;
+    }
+    string[] rez = new string[n];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            rez[j] = stringArray[i];
+            j++;
+        }
+    }
+    return rez;
+}
+
+
+
+void printA(string[] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"‘{ stringArray[i]}’, ");
+}
+Console.Write("]");
+}
+
+
+
+array(stringArray);
+printA(symbol(stringArray));
